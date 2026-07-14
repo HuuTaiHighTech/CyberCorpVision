7e69e580ab9b9170e7a2a51bffd1aaded2607603
tai <huutai.nguyen.dev@gmail.com>
2026-07-15 06:53:06 +0700

    task funtion backtotop done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 ﻿// file tổng hợp những file hiệu ứng
 import { initHeader } from "./effects/header.js";
 import { initHome } from "./effects/home.js";
-
+import { initBackToTop } from './effects/backtotop.js';
 document.addEventListener("DOMContentLoaded", () => {
     initHeader();
     initHome();
+    initBackToTop();
 });
